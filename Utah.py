c33daa9582e57cd4da3c02ac7bf36d6d1e3e0783
Stes315770 <stes315770@gmail.com>
2024-02-01 21:12:09 -0700

    Added criteria to py file
    Collab with group members in class.

--- Utah.py
+++ Utah.py
@@ -1,2 +1,21 @@
-def test_func():
-    print("Hello, World!")
+# Analysis and Conclusion:
+# Hypothesis: The average price of a home in Utah is higher over time due to a combination of rising incomes and population growth.
+# Questions: 
+# 1. What is the average price of a home in Utah over time?
+# 2. What is the average income in Utah over time?
+# 3. What is the population of Utah over time?
+# 4. What is the relationship between the average price of a home, average income, and population?
+
+# Importing the necessary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Reading in the data from FRED and storing it in a dataframe
+
+# Visualizing the Data:
+
+# Fig. 1 - Average Price of a Home in Utah Over Time
+# Fig. 2 - Average Income in Utah Over Time
+# Fig. 3 - Population of Utah Over Time
+# Fig. 4 - Relationship Between the Average Price of a Home, Average Income, and Population 
