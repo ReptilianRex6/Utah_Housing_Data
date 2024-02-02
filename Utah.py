d9c73ca2e8ad8095dc8933a1956aa617e606ccee
zakreez <karliebaxter@Karlies-MacBook-Pro.local>
2024-02-01 21:22:24 -0700

    maybe this works if I use the terminal.

--- Utah.py
+++ Utah.py
@@ -1,2 +1,7 @@
 def test_func():
     print("Hello, World!")
+git config --global user.name "zakreez"
+import pandas as pd
+HPI_monthly_file_path = 'HPI_PO_monthly_hist.xls'
+df_HPI = pd.read_excel(HPI_monthly_file_path)
+print(df_HPI.head())
