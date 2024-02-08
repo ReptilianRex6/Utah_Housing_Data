26566724bf578c9d4b9932d73b62b27fec23cfe3
zakreez <zacbaxter89@gmail.com>
2024-02-08 14:41:12 -0700

    Adding Prophecies folder to house work with Prophet

---
+++ Prophecies/Prophecy_work.py
@@ -0,0 +1,38 @@
+
+#Working on applying Prophet to our datasets
+
+#Importing the necessary libraries
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+from prophet import Prophet
+
+#Read in the data
+
+
+#Analyze the data
+
+
+#Plot general trends
+
+
+#Transform the data, if necessary
+
+
+
+#Prepare the data for Prophet
+
+
+#Train the model
+
+
+#Make predictions
+
+
+#Plot the predictions
+
+
+
+
+#Evaluate the model
